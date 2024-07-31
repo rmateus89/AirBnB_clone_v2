c90d677941fae1bb1953473eb494702013c632db
rmateus89 <raul.fm.mateus@gmail.com>
2024-07-31 21:07:40 +0000

    done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,15 +3,15 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_hbnb():
-    """print web"""
+@app.route('/', strict_slashes=False)
+def home():
+    """
+
+    """
     return 'Hello HBNB!'
 
-
+	
 if __name__ == '__main__':
-    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
